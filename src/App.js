211faac932b2b2bdfbb404959ecfb316f328cb99
Andrew Robles <andrewrobles@verizon.net>
2023-02-28 18:53:47 -0800

    Redirect to resume URL

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react'
+
+const url = 'https://bit.ly/andrew-robles-resume'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    window.location.replace(url)
+  }, [])
+  return <></>
 }
 
 export default App;
