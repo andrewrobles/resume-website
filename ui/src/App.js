e70008cdb112412016910f70fca6db76f283efe7
Andrew Robles <andrewrobles@verizon.net>
2023-03-04 10:52:51 -0800

    Add font

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,21 +1,7 @@
-import React, { useLayoutEffect, useState } from 'react';
-
-function useWindowSize() {
-  const [size, setSize] = useState([0, 0]);
-  useLayoutEffect(() => {
-    function updateSize() {
-      setSize([window.innerWidth, window.innerHeight]);
-    }
-    window.addEventListener('resize', updateSize);
-    updateSize();
-    return () => window.removeEventListener('resize', updateSize);
-  }, []);
-  return size;
-}
+import './App.css'
 
 function App () {
-  const [width, height] = useWindowSize();
-  return <iframe width="100%" height={height} src="https://drive.google.com/file/d/1nbOVZyS6nh_BH_Cr2e_gKmkv2e62OvO4/preview?usp=share_link" title="W3Schools Free Online Web Tutorials"></iframe>
+  return <h1 className="text">ANDREW ROBLES</h1>
 }
 
 export default App
