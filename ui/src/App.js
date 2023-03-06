d2c2e6706d250d5a6ae532bbce3d2df383a2b654
Andrew Robles <andrewrobles@verizon.net>
2023-03-05 21:39:19 -0800

    Start implementing section headers

--- ui/src/App.js
+++ ui/src/App.js
@@ -13,11 +13,34 @@ function App () {
     </div>
     <div className="mt-3 ml-4 text section-header">OBJECTIVE</div>
     <div className="mx-4 my-1 line"></div>
+
     <div className="mx-4 text content">Software Engineer with 3+ years of experience in JavaScript and Python, open to full-stack or front-end web development roles.</div>
     <div className="mt-3 ml-4 text section-header">EDUCATION</div>
     <div className="mx-4 my-1 line"></div>
-    <div className="mt-2 ml-4 text section-header">Full-Stack Developer</div>
-    <div className="mx-4 text content">Apple</div>
+    <div id="HASH" class="blue-msg">
+    <span id="time-HASH" class="smalltext"><span className="mt-2 ml-4 text section-header">Full-Stack Developer</span></span>
+    <span class="ios-circle"><span className="text content mr-4">6/22 - Present</span></span>
+    </div>
+    <div id="HASH" class="blue-msg">
+    <span id="time-HASH" class="smalltext"><span className="mx-4 text content">Apple</span></span>
+    <span class="ios-circle"><span className="text content mr-4">Culver City, CA</span></span>
+    </div>
+    <div className="mt-2 mx-4 text content">• Migrated front-end component library from Vue2 to Vue3 to allow half a dozen web apps to upgrade dependencies to latest version.</div>
+    <div className="mt-2 mx-4 text content">• Streamlined development workflow by decoupling API from cloud infrastructure by containerizing Kafka broker and MongoDB database.</div>
+    <div className="mt-2 mx-4 text content">• Created micro-service for consuming Kafka messages from software applications from various external departments.</div>
+    <div className="mt-2 mx-4 text content">• Defined API endpoints to allow frontend to communicate with the MongoDB database.</div>
+    <div id="HASH" class="blue-msg mt-2">
+    <span id="time-HASH" class="smalltext"><span className="mt-2 ml-4 text section-header">Full-Stack Developer</span></span>
+    <span class="ios-circle"><span className="text content mr-4">12/21 - 5/22</span></span>
+    </div>
+    <div id="HASH" class="blue-msg">
+    <span id="time-HASH" class="smalltext"><span className="mx-4 text content">StringKing</span></span>
+    <span class="ios-circle"><span className="text content mr-4">Gardena, CA</span></span>
+    </div>
+    <div className="mt-2 mx-4 text content">• Migrated front-end component library from Vue2 to Vue3 to allow half a dozen web apps to upgrade dependencies to latest version.</div>
+    <div className="mt-2 mx-4 text content">• Streamlined development workflow by decoupling API from cloud infrastructure by containerizing Kafka broker and MongoDB database.</div>
+    <div className="mt-2 mx-4 text content">• Created micro-service for consuming Kafka messages from software applications from various external departments.</div>
+    <div className="mt-2 mx-4 text content">• Defined API endpoints to allow frontend to communicate with the MongoDB database.</div>
   </>
 }
 
