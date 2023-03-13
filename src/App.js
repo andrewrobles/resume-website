d0ef2eae72235dba3d962f8d145c1dd72501905f
Andrew Robles <andrewrobles@verizon.net>
2023-03-12 21:16:52 -0700

    Revert "Make a full width iframe"
    This reverts commit f8a5759b8fcf9b2e3e1b770b9c5d76a09b4969ab.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
-import React, { useLayoutEffect, useState } from 'react';
+import { useEffect } from 'react'
 
-function useWindowSize() {
-  const [size, setSize] = useState([0, 0]);
-  useLayoutEffect(() => {
-    function updateSize() {
-      setSize([window.innerWidth, window.innerHeight]);
-    }
-    window.addEventListener('resize', updateSize);
-    updateSize();
-    return () => window.removeEventListener('resize', updateSize);
-  }, []);
-  return size;
+const url = 'https://bit.ly/andrew-robles-resume'
+
+function App() {
+  useEffect(() => {
+    window.location.replace(url)
+  }, [])
+  return <></>
 }
 
-function App () {
-  const [width, height] = useWindowSize();
-  return <iframe width="100%" height={height} src="https://drive.google.com/file/d/1nbOVZyS6nh_BH_Cr2e_gKmkv2e62OvO4/preview?usp=share_link" title="W3Schools Free Online Web Tutorials"></iframe>
-}
-
-export default App
+export default App;
